00cfd2acb7f9a8b5175a59c06032239c84fcd1bd
Josungsin <sungsin1030@gmail.com>
2023-06-23 16:35:19 +0900

    first commit

---
+++ src/main/java/com/example/authnexus/domain/User.java
@@ -0,0 +1,42 @@
+package com.example.authnexus.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user", schema = "user")
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idx", nullable = false)
+    private Long idx;
+
+    @Column(name = "user_id")
+    private String userId;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "gender")
+    private String gender;
+
+    @Column(name = "birthday")
+    private LocalDate birthday;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "tel")
+    private String tel;
+}
