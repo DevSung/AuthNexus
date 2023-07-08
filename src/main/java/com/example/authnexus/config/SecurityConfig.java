704a0ca50df9129b943bd519561ea20a23497189
Josungsin <sungsin1030@gmail.com>
2023-07-08 10:03:41 +0900

    SecurityConfig Post Api 오류 수정

--- src/main/java/com/example/authnexus/config/SecurityConfig.java
+++ src/main/java/com/example/authnexus/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.authnexus.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,9 +15,11 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/api/user/**").permitAll()
                 .antMatchers("/api/user/**").permitAll() // "/api/user/"로 시작하는 모든 경로에 대해 인증 없이 접근 허용
                 .anyRequest().authenticated() // 다른 모든 요청은 인증이 필요함
                 .and()
+                .csrf().disable()
                 .httpBasic();
         return http.build();
     }
