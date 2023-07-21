b87abe988d4763bfbe2a1ca1c2da8ae0a676577b
Josungsin <sungsin1030@gmail.com>
2023-07-21 17:50:29 +0900

    JUnit5 Test Code 추가

--- src/test/java/com/example/authnexus/service/MemberServiceTest.java
+++ src/test/java/com/example/authnexus/service/MemberServiceTest.java
@@ -1,32 +1,91 @@
 package com.example.authnexus.service;
 
+import com.example.authnexus.domain.member.Member;
 import com.example.authnexus.domain.member.repository.MemberRepository;
-import org.junit.jupiter.api.BeforeEach;
+import com.example.authnexus.domain.member.repository.MemberRoleRepository;
+import com.example.authnexus.payload.JwtToken;
+import com.example.authnexus.payload.LoginRequest;
+import com.example.authnexus.payload.MemberInfoResponse;
+import com.example.authnexus.security.JwtTokenProvider;
+import com.example.authnexus.service.impl.MemberServiceImpl;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
 @ActiveProfiles("local")
 @ExtendWith(MockitoExtension.class)
 class MemberServiceTest {
 
+    /**
+     * @Mock 모의 객체를 생성해준다.
+     * @InjectMocks 테스트 대상 객체에 모의 객체를 주입해준다.
+     */
+
     @Mock
     MemberRepository memberRepository;
 
+    @Mock
+    MemberRoleRepository memberRoleRepository;
+
+    @Mock
+    Argon2PasswordEncoder passwordEncoder;
+
+    @Mock
+    JwtTokenProvider jwtTokenProvider;
+
     @InjectMocks
-    MemberService memberService;
+    MemberServiceImpl memberService;
 
     @Test
-    @DisplayName("회원가입 테스트")
-    void joinTest() {
+    @DisplayName("토큰 발급 테스트")
+    void loginTest() {
+        // given
+        String pw = passwordEncoder.encode("1234");
+        Member member = Member.builder()
+                .idx(1L)
+                .userId("test")
+                .password(pw)
+                .build();
 
+        LoginRequest loginRequest = new LoginRequest();
+        loginRequest.setUserId(member.getUserId());
+        loginRequest.setPassword("1234");
 
+        Optional<Member> memberOptional = Optional.of(member);
+
+        // stub
+        when(memberRepository.findByUserId(any())).thenReturn(memberOptional);
+        when(passwordEncoder.matches("1234", pw)).thenReturn(true);
+
+        JwtToken jwtToken = JwtToken.builder()
+                .accessToken("ddd")
+                .refreshToken("dddd")
+                .build();
+
+        when(jwtTokenProvider.generateToken(memberOptional.get())).thenReturn(jwtToken);
+
+        // when
+        JwtToken result = memberService.login(loginRequest);
+
+        // then
+        System.out.println(result.getAccessToken());
+    }
+
+    @Test
+    @DisplayName("회원목록")
+    void getUser() {
+        List<MemberInfoResponse> infos = memberService.getUsers();
     }
 
 }
