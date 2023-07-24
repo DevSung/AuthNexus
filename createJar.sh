86fd638239992d1f73722855e27cb868332b6df0
Josungsin <sungsin1030@gmail.com>
2023-07-24 11:28:24 +0900

    createJar.sh 수정 서버 배포 테스트

--- createJar.sh
+++ createJar.sh
@@ -13,37 +13,12 @@ elif [ "$PROFILE_GROUP" != "local" ] && [ "$PROFILE_GROUP" != "dev" ] && [ "$PRO
 fi
 
 # root
-ROOT_PATH=/Users/smart-tn-055/AuthNexus
+ROOT_PATH=/home/sungsin/java_source
 
 SERVER_USER=sungsin
-SERVER_PATH=20.249.85.149
-SERVER_KEY=/Users/smart-tn-055/server_key.pem
+SERVER_PATH=${{ secrets.SERVER_HOST }}
+SERVER_KEY=${{ secrets.SERVER_KEY }}
 PORT=8003
-
-# 디렉토리 이동
-cd $ROOT_PATH || {
-  echo "디렉토리가 존재하지 않습니다."
-  exit 1
-}
-
-# .jar 생성
-./gradlew build || {
-  echo "jar 파일 생성에 실패했습니다."
-  exit 1
-}
-
-# .jar 파일 경로
-JAR_PATH="$ROOT_PATH/build/libs/*.jar"
-
-if [ "$PROFILE_GROUP" = "prod" ]; then
-  scp -i $SERVER_KEY $JAR_PATH $SERVER_USER@$SERVER_PATH:/home/sungsin/java_source
-
-  if [ $? -eq 0 ]; then
-    echo "jar 파일을 개인 서버로 전송했습니다."
-  else
-    echo "jar 파일 전송에 실패했습니다."
-    exit 1
-  fi
 
 # port 확인
 PID=$(ssh -i $SERVER_KEY $SERVER_USER@$SERVER_PATH "lsof -t -i :$PORT")
@@ -54,10 +29,9 @@ if [ -n "$PID" ]; then
   sleep 3
 fi
 
-ssh -i $SERVER_KEY $SERVER_USER@$SERVER_PATH "java -jar /home/sungsin/java_source/*.jar --spring.profiles.active=$PROFILE_GROUP"
+ssh -i $SERVER_KEY $SERVER_USER@$SERVER_PATH "java -jar build/libs/*.jar --spring.profiles.active=prod"
 echo "$PROFILE_GROUP jar 파일이 실행됐습니다."
 
-fi
 
 
 
