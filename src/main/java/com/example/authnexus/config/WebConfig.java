28d7295d2c823305b0cd9c2194efe707704ee824
Josungsin <sungsin1030@gmail.com>
2023-07-25 18:05:17 +0900

    SecurityConfig.java cors 설정 수정

---
+++ src/main/java/com/example/authnexus/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.example.authnexus.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // 허용할 출처를 여기에 설정
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Authorization", "Authorization-refresh", "Cache-Control", "Content-Type")
+                .allowCredentials(true);
+    }
+}
